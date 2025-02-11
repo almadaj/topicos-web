5c5719caa50b0dee6a0cd6102fa9655195ddc10c
Juliano Almada <julianoalmada48@gmail.com>
2025-02-10 22:49:26 -0300

    simple commit

--- src/br/edu/unichristus/Main.java
+++ src/br/edu/unichristus/Main.java
@@ -2,6 +2,6 @@ package br.edu.unichristus;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Showing diffs");
+        System.out.println("Another diffs");
     }
 }
